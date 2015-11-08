18ad96ed078406ab12ea04cfc8a0b5b1f0d2b9ac
Anthony Kuske <anthonykuske@gmail.com>
2015-11-08 07:16:53 +0000

    Add license

--- src/Models/Service.php
+++ src/Models/Service.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CloudScaler package.
+ *
+ * (c) Anthony Kuske <www.anthonykuske.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Tmd\CloudScaler\Models;
 
 use Tmd\CloudScaler\Providers\Dns\DnsProvider;
